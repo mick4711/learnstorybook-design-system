56d0d9c60d4198276dc35a47761d63f08116a068
mick4711 <mick4711@gmail.com>
2021-11-03 19:38:44 +0000

    update button comments

--- src/Button.js
+++ src/Button.js
@@ -316,6 +316,10 @@ const applyStyle = ButtonWrapper => {
   );
 };
 
+/**
+- Use an avatar for attributing actions or content to specific users.
+- The user's name should always be present when using Avatar – either printed beside the avatar or in a tooltip.
+**/
 export function Button({
   isDisabled,
   isLoading,
